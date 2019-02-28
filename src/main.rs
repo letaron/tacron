9155f5ea0e02fbd3f7e7053abd8ab35820eb362d
tom <t.vitureau@purjus.fr>
2019-03-01 00:30:44 +0100

    add crontab reader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate regex;
+use regex::Regex;
 
 #[derive(Debug)]
 struct TaCron {
@@ -7,16 +8,17 @@ struct TaCron {
     dom: String,
     month: String,
     dow: String,
-    action: String
+    action: String,
+    origin: String
 }
 
 trait Reader {
-    fn read() -> Vec<TaCron>;
+    fn read(&self) -> Vec<TaCron>;
 }
 
 impl TaCron {
-    fn new(minute: String, hour: String, dom: String, month: String, dow: String, action: String) -> TaCron {
-        TaCron {minute: minute, hour: hour, dom: dom, month: month, dow: dow, action: action}
+    fn new(minute: String, hour: String, dom: String, month: String, dow: String, action: String, origin: String) -> TaCron {
+        TaCron {minute: minute, hour: hour, dom: dom, month: month, dow: dow, action: action, origin: origin}
     }
 }
 
@@ -24,30 +26,47 @@ struct CrontabReader {
 }
 
 impl Reader for CrontabReader {
-    fn read() -> Vec<TaCron> {
-        let line = "0\t2          12             *                *            /usr/bin/find".to_string();
-        let re = regex::Regex::new(r"\s+").unwrap();
-        let data = re.split(&line);
-        let cron: Vec<&str> = data.collect();
+    fn read(&self) -> Vec<TaCron> {
         let mut tasks: Vec<TaCron> = Vec::new();
+        let lines = "0\t2 12 * *  /usr/bin/find\n1 2 3 4 5 ls\n#comment".to_string();
 
-        tasks.push( 
-            TaCron::new(
-                cron[0].to_string(),
-                cron[1].to_string(),
-                cron[2].to_string(),
-                cron[3].to_string(),
-                cron[4].to_string(),
-                cron[5].to_string()
-                )
-            );
+        let comment_regex = Regex::new(r"^\s?#").unwrap();
+
+        for line in lines.split("\n") {
+
+            if comment_regex.is_match(line) == true {
+                continue;
+            }
+
+            tasks.push( 
+                tacron_from_crontab_line(line.to_string())
+                );
+        }
 
         tasks
     }
+
+
+}
+
+fn tacron_from_crontab_line(line: String) -> TaCron {
+    let re = Regex::new(r"\s+").unwrap();
+    let data = re.split(&line);
+    let cron: Vec<&str> = data.collect();
+    TaCron::new(
+        cron[0].to_string(),
+        cron[1].to_string(),
+        cron[2].to_string(),
+        cron[3].to_string(),
+        cron[4].to_string(),
+        cron[5].to_string(),
+        "origin".to_string()
+        )
 }
 
 fn main() {
-    let tasks = CrontabReader::read();
+    let reader = CrontabReader {};
+    let tasks = reader.read();
     for task in tasks {
         println!("{:?}", task);
     }
