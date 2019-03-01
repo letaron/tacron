9b1a1dab1c3641ba33d02bb5952543421bf52605
tom <t.vitureau@purjus.fr>
2019-03-01 10:32:16 +0100

    crontab reader as a module

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-extern crate regex;
-use regex::Regex;
+mod reader;
+use reader::crontab_reader::CrontabReader;
 
 #[derive(Debug)]
 struct TaCron {
@@ -12,61 +12,19 @@ struct TaCron {
     origin: String
 }
 
-trait Reader {
-    fn read(&self) -> Vec<TaCron>;
-}
-
 impl TaCron {
     fn new(minute: String, hour: String, dom: String, month: String, dow: String, action: String, origin: String) -> TaCron {
         TaCron {minute: minute, hour: hour, dom: dom, month: month, dow: dow, action: action, origin: origin}
     }
 }
 
-struct CrontabReader {
-}
-
-impl Reader for CrontabReader {
-    fn read(&self) -> Vec<TaCron> {
-        let mut tasks: Vec<TaCron> = Vec::new();
-        let lines = "0\t2 12 * *  /usr/bin/find\n1 2 3 4 5 ls\n#comment".to_string();
-
-        let comment_regex = Regex::new(r"^\s?#").unwrap();
-
-        for line in lines.split("\n") {
-
-            if comment_regex.is_match(line) == true {
-                continue;
-            }
-
-            tasks.push( 
-                tacron_from_crontab_line(line.to_string())
-                );
-        }
-
-        tasks
-    }
-
-
-}
-
-fn tacron_from_crontab_line(line: String) -> TaCron {
-    let re = Regex::new(r"\s+").unwrap();
-    let data = re.split(&line);
-    let cron: Vec<&str> = data.collect();
-    TaCron::new(
-        cron[0].to_string(),
-        cron[1].to_string(),
-        cron[2].to_string(),
-        cron[3].to_string(),
-        cron[4].to_string(),
-        cron[5].to_string(),
-        "origin".to_string()
-        )
+trait Reader {
+    fn read(&self) -> Vec<TaCron>;
 }
 
 fn main() {
-    let reader = CrontabReader {};
-    let tasks = reader.read();
+    let tasks_reader = CrontabReader {};
+    let tasks = tasks_reader.read();
     for task in tasks {
         println!("{:?}", task);
     }
---
+++ src/reader/mod.rs
@@ -0,0 +1,1 @@
+pub mod crontab_reader;
