387eaff48849699e1eb602ad6385a4ad5f5b712f
tom <t.vitureau@purjus.fr>
2019-03-01 17:30:26 +0100

    add tests & special strings in cron

--- src/reader/crontab_reader.rs
+++ src/reader/crontab_reader.rs
@@ -28,15 +28,69 @@ impl Reader for CrontabReader {
 
             let data = line_regex.split(line);
             let cron: Vec<&str> = data.collect();
-            tasks.push(TaCron::new(
-                cron[0].to_string(),
-                cron[1].to_string(),
-                cron[2].to_string(),
-                cron[3].to_string(),
-                cron[4].to_string(),
-                cron[5].to_string(),
-                self.file.to_string(),
-            ));
+
+            if line.chars().next() == Some('@') {
+                let ta_cron = match cron[0] {
+                    "@yearly" | "@annually" => TaCron::new(
+                        "0".to_string(),
+                        "0".to_string(),
+                        "1".to_string(),
+                        "1".to_string(),
+                        "*".to_string(),
+                        cron[1..].join(" "),
+                        self.file.to_string(),
+                    ),
+                    "@monthly" => TaCron::new(
+                        "0".to_string(),
+                        "0".to_string(),
+                        "1".to_string(),
+                        "*".to_string(),
+                        "*".to_string(),
+                        cron[1..].join(" "),
+                        self.file.to_string(),
+                    ),
+                    "@weekly" => TaCron::new(
+                        "0".to_string(),
+                        "0".to_string(),
+                        "*".to_string(),
+                        "*".to_string(),
+                        "0".to_string(),
+                        cron[1..].join(" "),
+                        self.file.to_string(),
+                    ),
+                    "@daily" | "@midnight" => TaCron::new(
+                        "0".to_string(),
+                        "0".to_string(),
+                        "*".to_string(),
+                        "*".to_string(),
+                        "*".to_string(),
+                        cron[1..].join(" "),
+                        self.file.to_string(),
+                    ),
+                    "@hourly" => TaCron::new(
+                        "0".to_string(),
+                        "*".to_string(),
+                        "*".to_string(),
+                        "*".to_string(),
+                        "*".to_string(),
+                        cron[1..].join(" "),
+                        self.file.to_string(),
+                    ),
+                    x => panic!("Invalid crontab value: {}", x),
+                };
+
+                tasks.push(ta_cron);
+            } else {
+                tasks.push(TaCron::new(
+                    cron[0].to_string(),
+                    cron[1].to_string(),
+                    cron[2].to_string(),
+                    cron[3].to_string(),
+                    cron[4].to_string(),
+                    cron[5..].join(" "),
+                    self.file.to_string(),
+                ));
+            }
         }
 
         tasks
@@ -56,7 +110,7 @@ mod tests {
         };
 
         let tasks = reader.read();
-        assert_eq!(tasks.len(), 2);
+        assert_eq!(tasks.len(), 9);
 
         let mut task = &tasks[0];
         assert_eq!(task.minute, "0");
@@ -73,7 +127,7 @@ mod tests {
         assert_eq!(task.dom, "3,4");
         assert_eq!(task.month, "4");
         assert_eq!(task.dow, "5");
-        assert_eq!(task.action, "baz");
+        assert_eq!(task.action, "baz \"foo\" 2>&1");
         assert_eq!(task.origin, origin);
     }
 }
