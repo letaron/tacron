e9834cf8a8c8905cc8898233f6adb412b3e09c7a
tom <t.vitureau@purjus.fr>
2019-03-18 22:58:24 +0100

    extract parse reader function

--- src/reader/file_reader.rs
+++ src/reader/file_reader.rs
@@ -14,61 +14,65 @@ impl FileReader {
     }
 }
 
-impl Reader for FileReader {
-    fn raw_crons(&self) -> Vec<RawCron> {
-        let mut tasks: Vec<RawCron> = Vec::new();
-        let content = fs::read_to_string(&self.file).expect("Unable to read file");
+fn parse_content(content: String, name: &String) -> Vec<RawCron> {
+    let mut tasks: Vec<RawCron> = Vec::new();
+    let line_is_comment_regex = Regex::new(r"^\s*#").unwrap();
+    let line_split_regex = Regex::new(r"\s+").unwrap();
 
-        let line_is_comment_regex = Regex::new(r"^\s*#").unwrap();
-        let line_split_regex = Regex::new(r"\s+").unwrap();
+    let mut line_number = 0;
+    for line in content.split("\n") {
+        line_number += 1;
+        let source = format!("file_reader@{}:{}", name, line_number);
 
-        let mut line_number = 0;
-        for line in content.split("\n") {
-            line_number += 1;
-            let source = format!("file_reader@{}:{}", self.file.to_string(), line_number);
-
-            if line.len() == 0 || line_is_comment_regex.is_match(line) {
-                continue;
-            }
-
-            let cron: Vec<&str> = line_split_regex.split(line).collect();
-
-            let times_specs;
-            let command_index;
-
-            if line.chars().next() == Some('@') {
-                command_index = 1;
-                times_specs = match cron[0] {
-                    "@yearly" | "@annually" => ["0", "0", "1", "1", "*"],
-                    "@monthly" => ["0", "0", "1", "*", "*"],
-                    "@weekly" => ["0", "0", "*", "*", "0"],
-                    "@daily" | "@midnight" => ["0", "0", "*", "*", "*"],
-                    "@hourly" => ["0", "*", "*", "*", "*"],
-                    x => {
-                        println!(
-                            "{}",
-                            format!("[WARNING] {} - Invalid special value: {}", source, x)
-                        );
-                        continue;
-                    }
-                };
-            } else {
-                command_index = 5;
-                times_specs = [cron[0], cron[1], cron[2], cron[3], cron[4]]
-            }
-
-            tasks.push(RawCron::new(
-                times_specs[0].to_string(),
-                times_specs[1].to_string(),
-                times_specs[2].to_string(),
-                times_specs[3].to_string(),
-                times_specs[4].to_string(),
-                cron[command_index..].join(" ").to_string(),
-                source,
-            ));
+        if line.len() == 0 || line_is_comment_regex.is_match(line) {
+            continue;
         }
 
-        tasks
+        let cron: Vec<&str> = line_split_regex.split(line).collect();
+
+        let times_specs;
+        let command_index;
+
+        if line.chars().next() == Some('@') {
+            command_index = 1;
+            times_specs = match cron[0] {
+                "@yearly" | "@annually" => ["0", "0", "1", "1", "*"],
+                "@monthly" => ["0", "0", "1", "*", "*"],
+                "@weekly" => ["0", "0", "*", "*", "0"],
+                "@daily" | "@midnight" => ["0", "0", "*", "*", "*"],
+                "@hourly" => ["0", "*", "*", "*", "*"],
+                x => {
+                    println!(
+                        "{}",
+                        format!("[WARNING] {} - Invalid special value: {}", source, x)
+                    );
+                    continue;
+                }
+            };
+        } else {
+            command_index = 5;
+            times_specs = [cron[0], cron[1], cron[2], cron[3], cron[4]]
+        }
+
+        tasks.push(RawCron::new(
+            times_specs[0].to_string(),
+            times_specs[1].to_string(),
+            times_specs[2].to_string(),
+            times_specs[3].to_string(),
+            times_specs[4].to_string(),
+            cron[command_index..].join(" ").to_string(),
+            source,
+        ));
+    }
+
+    tasks
+}
+
+impl Reader for FileReader {
+    fn raw_crons(&self) -> Vec<RawCron> {
+        let content = fs::read_to_string(&self.file).expect("Unable to read file");
+
+        parse_content(content, &self.file)
     }
 }
 
